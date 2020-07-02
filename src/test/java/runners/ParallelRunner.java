52eee35ddfb6d9f09dbabeeb14ee2000e21b729f
Ernest Bolot <ernstin@gmail.com>
2020-07-02 15:54:31 -0500

    d

--- src/test/java/runners/ParallelRunner.java
+++ src/test/java/runners/ParallelRunner.java
@@ -7,8 +7,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-        "pretty", "html:target/cucumber.html",
-        "json:target/cucumber.json","extent:target/report.html"
+                "pretty",
+                "html:target/cucumber.html",
+                "json:target/cucumber.json"
         },
         features = "src/test/resources/features",
         glue = "steps")
